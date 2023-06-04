c11683d7669c8616e34cbe240f0d426b5b956780
Luiz Otávio Dos Santos <luizotavio.03@outlook.com>
2023-06-04 15:18:21 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,22 @@
 function getProjects(){
-    const urlGitHub = 'https://github.com/luizotvio?tab=repositories'
+    const urlGitHub = 'https://api.github.com/users/luizotvio/repos'
+    var loadingElement = document.getElementById('loading')
 
     fetch(urlGitHub, {
         method: 'GET'
     })
         .then((response) => response.json())
         .then((response) => {
-            console.log(response)
+            loadingElement.style.display = 'none'
+            showProjects(response)
         })
         .catch((e) => {
             console.log(e)
         })
+}
+
+function showProjects(data){
+
 }
 
 getProjects()
